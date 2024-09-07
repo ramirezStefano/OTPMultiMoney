06b57e7d5dae8411c1333229d64ca50002087411
Stefano Ramirez <ramirez.stefano@gmail.com>
2024-09-07 11:30:00 -0600

    add: more tests

--- src/components/OTPForm/OTPForm.test.tsx
+++ src/components/OTPForm/OTPForm.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { describe, expect, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { OTPForm } from './OTPForm'
 
 describe('OTPForm Component', () => {
-  test('OTP renders on screen', () => {
+  test('OTP Header renders on screen', () => {
     render(<OTPForm formFieldAmount={4} expectedOTP="1234" />) // buggy
     const element = screen.getByText(/Te enviamos un SMS/i)
     expect(element).toBeTruthy()
@@ -13,5 +13,15 @@ describe('OTPForm Component', () => {
     render(<OTPForm formFieldAmount={4} expectedOTP="1234" />) //
     const element = screen.getByText(/Continuar/i)
     expect(element).toBeTruthy()
+  })
+  test('Input field renders correctly', () => {
+    render(<OTPForm formFieldAmount={4} expectedOTP="1234" />)
+    const inputElements = screen.getAllByRole('textbox')
+    expect(inputElements.length).toBe(4)
+  })
+  test('Input field has a maximum length of 1', () => {
+    render(<OTPForm formFieldAmount={1} expectedOTP="1" />)
+    const inputElement = screen.getByRole('textbox') as HTMLInputElement
+    expect(inputElement.maxLength).toBe(1)
   })
 })
