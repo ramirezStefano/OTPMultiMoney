f447b859ba9d49e4385fa69d66b2b9e338985cfc
Stefano Ramirez <ramirez.stefano@gmail.com>
2024-09-07 11:51:19 -0600

    add: more tests and tsIgnore on required React module for tests.

--- src/components/OTPForm/OTPForm.test.tsx
+++ src/components/OTPForm/OTPForm.test.tsx
@@ -1,16 +1,17 @@
+// @ts-ignore
 import React from 'react'
 import { describe, expect, test } from 'vitest'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { OTPForm } from './OTPForm'
 
 describe('OTPForm Component', () => {
   test('OTP Header renders on screen', () => {
-    render(<OTPForm formFieldAmount={4} expectedOTP="1234" />) // buggy
+    render(<OTPForm formFieldAmount={4} expectedOTP="1234" />)
     const element = screen.getByText(/Te enviamos un SMS/i)
     expect(element).toBeTruthy()
   })
   test('Button renders on screen', () => {
-    render(<OTPForm formFieldAmount={4} expectedOTP="1234" />) //
+    render(<OTPForm formFieldAmount={4} expectedOTP="1234" />)
     const element = screen.getByText(/Continuar/i)
     expect(element).toBeTruthy()
   })
